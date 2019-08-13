3dddf3065e22bf9b03599a6389ad0d221d7c9223
willian matheus <willian_matheus14@hotmail.com>
2019-08-13 01:52:13 -0300

    initial commit

---
+++ FallenWinged/Assets/Scripts/ApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //in case that we want to put a exit button
+    public static void ExitGame()
+    {
+        Application.Quit();
+    }
+}
