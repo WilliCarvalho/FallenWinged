5ff8285c1c3fa57964f849b5d46de2d4553e34c1
Willian Matheus <willian_matheus14@hotmail.com>
2019-08-26 23:08:01 -0300

    estructuring character selection class/stats

--- FallenWinged/Assets/Scripts/PlayerScript/PlayerBehavior.cs
+++ FallenWinged/Assets/Scripts/PlayerScript/PlayerBehavior.cs
@@ -17,7 +17,7 @@ public class PlayerBehavior : CharacterBase
     //private AnimationController animationController;
 
     // Start is called before the first frame update
-    protected new void Start()
+    protected void Start()
     {
         base.Start();
         currentLevel = PlayerStatsController.GetCurrentLevel();
@@ -43,5 +43,21 @@ public class PlayerBehavior : CharacterBase
         //{
         //    animationController.PlayAnimation(AnimationStates.IDLE);
         //}
+    }
+
+
+    //perguntar pro rodrigo como fazer escolha da classe do player por 1 método só
+    public void SetPlayerType()
+    {
+        PlayerStatsController.SetTyperCharacter(TypeCharacter.Killer);
+        type = PlayerStatsController.GetTypeCharacter();
+        basicStats = PlayerStatsController.instance.GetBasicStats(type);
+    }
+
+    public void SetPlayer()
+    {
+        PlayerStatsController.SetTyperCharacter(TypeCharacter.Palestrinha);
+        type = PlayerStatsController.GetTypeCharacter();
+        basicStats = PlayerStatsController.instance.GetBasicStats(type);
     }
 }
