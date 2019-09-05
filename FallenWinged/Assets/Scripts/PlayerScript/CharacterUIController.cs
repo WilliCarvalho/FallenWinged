da9d8763372810d98142ce31915569425fb1f449
WilliCarvalho <willian_matheus14@hotmail.com>
2019-09-04 22:43:23 -0300

    begin the addition of LevelUpMechanics
    When the player LevelUp he gets attributes points to put in his stats

---
+++ FallenWinged/Assets/Scripts/PlayerScript/CharacterUIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterUIController : MonoBehaviour
+{
+    public Text concentrationValue;
+    public Text comunicationValue;
+    public Text precisionValue;
+    public Text reflexValue;
+
+    public GameObject buttonAddConcentration;
+    public GameObject buttonAddComunication;
+    public GameObject buttonAddPrecision;
+    public GameObject buttonAddReflex;
+
+    public Text remainingPoints;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ShowButtons()
+    {
+        bool hasToShowButton = PlayerStatsController.GetAttributsPoints() > 0;
+
+        buttonAddConcentration.SetActive(hasToShowButton);
+        buttonAddComunication.SetActive(hasToShowButton);
+        buttonAddPrecision.SetActive(hasToShowButton);
+        buttonAddReflex.SetActive(hasToShowButton);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ShowButtons();
+        remainingPoints.text = PlayerStatsController.GetAttributsPoints().ToString();
+    }
+
+    public void AddAttribute(string attributeName)
+    {
+        PlayerStatsController.AddAttribute(attributeName, 1);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public bool HasWindowOpen()
+    {
+        return false;
+    }
+}
